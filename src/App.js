5e7a16c730bb15c83366abe9e81430ca3eb1825e
MirsonCamadro <kztempus@gmail.com>
2020-05-16 12:51:15 -0400

    added components CustomerEdit, CustomersList and CustomerListItem

--- src/App.js
+++ src/App.js
@@ -6,7 +6,8 @@ function App() {
   return (
     <Router>
       <div className="App">
-        <Link to="/customers">Customers</Link>
+        <Link to="/customers">Customers</Link><br></br>
+        <Link to="/customers/30000000">Customers 30.000.000</Link>
       </div>
     </Router>
   );
